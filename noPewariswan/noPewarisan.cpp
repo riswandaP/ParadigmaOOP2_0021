9eebd9ac3815cd937fd9540d33656282a4a26ea6
riswandaP <riswandaputra03@gmail.com>
2025-06-02 10:57:09 +0700

    menambahkan class derivedClass

--- noPewariswan/noPewarisan.cpp
+++ noPewariswan/noPewarisan.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class baseClass
+class baseClass 
 {
     public:
-        virtual void perkenalan()
+        virtual void perkenalan() 
         {
             cout << "Hallo saya Function dari base class";
         }
 };
+
+class derivedClass : public baseClass
+{
+    public:
+        void perkenalan()
+        {
+            cout << "Hallo saya Function dari derived class";
+        }
+};
+
