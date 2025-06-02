c201b3b659584f88b4abc1bb02826d5317090ba2
riswandaP <riswandaputra03@gmail.com>
2025-06-02 10:14:09 +0700

    membuat function int main

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -15,3 +15,12 @@ class remotLampu
             return saklarNo[i];
         }
 };
+
+int main()
+{
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+}
