a4d592d6f390305b01aa943fc1df5c2a75b99abb
riswandaP <riswandaputra03@gmail.com>
2025-06-02 11:10:22 +0700

    membuat class joko

--- polimorphi/polimorpi.cpp
+++ polimorphi/polimorpi.cpp
@@ -9,3 +9,12 @@ class seseorang
         //  cout <<"Pesan dari seseorang" << endl;
         //  }
 };
+
+class joko : public seseorang 
+{
+    public:
+    void pesan() 
+    {
+        cout <<"Pesan dari lia" << endl;
+    }
+};
