d592d55c5caf45f6ef32527f1d3e629d506d03d7
riswandaP <riswandaputra03@gmail.com>
2025-06-02 10:02:20 +0700

    membuat void display pada class AbstaksiKlas

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -15,4 +15,11 @@ class AbstraksiKlas
             y = b;
         }
         //menampilkan nilai 
+        void display() 
+        {
+            cout << "x = " << x << endl;
+            cout << "y = " << y << endl;
+        }
 }
+
+
