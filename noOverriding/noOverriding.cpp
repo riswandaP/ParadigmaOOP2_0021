b1743db80654d12dec62a35d4b1be42ac7d5b5d5
riswandaP <riswandaputra03@gmail.com>
2025-06-02 10:48:23 +0700

    membuat function int main

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -17,4 +17,13 @@ class derivedClass : public baseClass
         {
             cout << "Hallo saya Function dari derived class";
         }
+};
+
+int main()
+{
+    derivedClass a;
+    
+    a.perkenalan();
+
+    return 0;
 }
