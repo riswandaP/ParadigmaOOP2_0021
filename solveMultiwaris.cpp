19c2448363289fd5641e115b76daeb13262833b4
riswandaP <riswandaputra03@gmail.com>
2026-05-19 19:52:20 +0700

    Membuat class budi

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -30,3 +30,11 @@ public:
     }
 };
 
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) : 
+        pekerja(pUmur),
+        pelajar(pUmur) {
+            cout << "budi dibuat\n " << endl;
+    }
+};
